717d9c40ee7932817646c6aec314b5981f283c87
Jenny Lian <jennylian100@gmail.com>
2016-03-24 13:46:49 -0700

    mm try to modify try2

---
+++ 200x/280/inodePrint/try_dir.c
@@ -0,0 +1,60 @@
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+#include <string.h>
+
+
+
+static void lookup(const char *arg)
+{
+    DIR *dirp;
+    struct dirent *dp;
+
+
+    if ((dirp = opendir(arg)) == NULL) {
+        perror("couldn't open '.'");
+        return;
+    }
+
+
+    do {
+        errno = 0;
+        if ((dp = readdir(dirp)) != NULL) {
+            if (strcmp(dp->d_name, arg) != 0)
+                continue;
+
+
+            (void) printf("found %s\n", arg);
+            (void) closedir(dirp);
+                return;
+
+
+        }
+    } while (dp != NULL);
+
+
+    if (errno != 0)
+        perror("error reading directory");
+    else
+        (void) printf("failed to find %s\n", arg);
+    (void) closedir(dirp);
+    return;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int i;
+    for (i = 1; i < argc; i++)
+        lookup(argv[i]);
+    return (0);
+}
